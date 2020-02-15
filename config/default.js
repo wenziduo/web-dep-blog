608f044dfef3ed033731d7f10afbb951147227d6
caiwenduo <caiwenduo@163.com>
2020-02-15 17:01:32 +0800

    修改转发ip地址

--- config/default.js
+++ config/default.js
@@ -1,13 +1,13 @@
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 exports.serverIp =
   (process.env.NODE_ENV === 'production') ?
-    '48.98.50.170' :
+    '39.105.181.82:8019' :
       (
         process.env.NODE_ENV === 'test' ?
-          '48.98.50.170' :
+          '39.105.181.82:8019' :
             (
               process.env.NODE_ENV === 'development' ?
-                '48.98.50.170' :
+                '39.105.181.82:8019' :
                   ''
             )
       )
