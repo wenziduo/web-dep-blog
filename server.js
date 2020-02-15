19114402fcf1d0c2f7041efe55e99f8f826a406c
caiwenduo <caiwenduo@163.com>
2020-02-16 01:40:50 +0800

    修改server文件

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { serverIp } = require('./config/default')
 
 const devProxy = {
   '/api': {
-    target: `//${serverIp}/api`, // 目标服务器 host
+    target: `http://${serverIp}/api`, // 目标服务器 host
     pathRewrite: { '^/api': '/' }, // 重写请求，比如我们源访问的是api/login，那么请求会被解析为/www/login
     changeOrigin: true // 默认false，是否需要改变原始主机头为目标URL
   }
